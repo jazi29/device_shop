cdb00bd1559e464473852c99b1f19ed5071f9b3c
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2025-09-26 15:48:49 +0500

    Add files via upload

---
+++ app/buttons/page.tsx
@@ -0,0 +1,11 @@
+import { ButtonTabs } from "@/components/button-tabs"
+
+export default function ButtonsPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <div className="container mx-auto py-8">
+        <ButtonTabs />
+      </div>
+    </div>
+  )
+}
